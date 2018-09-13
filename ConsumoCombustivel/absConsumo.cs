40de1918aca46861c025225755032dd440e9cbea
Pedro Henrique Gonçalves Ramos <pedro-minato1@hotmail.com>
2018-09-12 22:46:41 -0300

    first commit

---
+++ ConsumoCombustivel/absConsumo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumoCombustivel
+{
+    public abstract class absConsumo
+    {
+        public double Consumo { get; set; }
+        public double Combustivel { get; set; }
+        public string Mensagem { get; set; }
+    }
+}
